30900d3211a544b3de2f4266ecba95a9bdbd87e7
clement <clement.lhommeau@hotmail.fr>
2012-11-26 17:47:42 +0100

    removing useless file and images
    themes and images have been removed

--- themes/agregateur/sidebar.php
+++
@@ -1,7 +0,0 @@
-<!-- BEGIN sidebar -->
-<div id="sidebar">
-	<!-- begin subscribe -->	
-	<div class="pad">
-	<p><img src="<?php bloginfo('template_url'); ?>/images/anim.jpg"  /></p>
-		</div>
-</div>
